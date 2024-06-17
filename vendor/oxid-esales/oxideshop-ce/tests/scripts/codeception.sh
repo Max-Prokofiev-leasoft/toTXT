ed4d942e2af49adc8900175c9e19a5873bc43da9
Max Prokofiev <max.prokofiev@leasoft.org>
2024-06-18 01:29:41 +0300

    sync

--- vendor/oxid-esales/oxideshop-ce/tests/scripts/codeception.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-vendor/bin/codecept build \
-    -c tests/codeception.yml
-vendor/bin/codecept run acceptanceSetup \
-    -c tests/codeception.yml \
-    --ext DotReporter 2>&1 \
-| tee tests/Output/codeception_ShopSetup.txt
--- vendor/oxid-esales/oxideshop-ce/tests/scripts/integration.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-vendor/bin/phpunit \
-    -c phpunit.xml \
-    --bootstrap tests/bootstrap.php \
-    --coverage-clover=tests/Reports/coverage_phpunit_integration.xml \
-    tests/Integration 2>&1 \
-| tee tests/Output/integration_tests.txt
